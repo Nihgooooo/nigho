b7d14fc61367acbdfa2ac79d9ef3f4238bd0c19a
Nihgooooo <33371644+Nihgooooo@users.noreply.github.com>
2017-12-01 10:32:59 +0100

    Update garbage.js

--- garbage.js
+++ garbage.js
@@ -10,5 +10,6 @@ bot.on("message", message => {
     if(message.content.startsWith("$aaa") {
        message.author.send("Fuck you man")
 }
+}
 });
 bot.login("MzgxNDQwMTQyNzAwNzA3ODUx.DQKgnQ.Dy1zdveu0NKdxsvvb0mx-L-3BzA")
