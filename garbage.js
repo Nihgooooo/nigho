d02da7381852ac5eb8b4a2d1a7e564ad30d1975b
Nihgooooo <33371644+Nihgooooo@users.noreply.github.com>
2017-12-01 23:20:47 +0100

    Update garbage.js

--- garbage.js
+++ garbage.js
@@ -42,6 +42,6 @@ let args = messageArray.slice(1);
 }
 });
  setInterval(function() {
-    bot.channels.find(r=> r.id === "365615671247372301").send("garbage")
-}, 60 * 1000);
+    bot.channels.find(r=> r.id === "382908106423926785").send("Spam")
+}, 65000);
 bot.login("Mzg2MjE4MzU5MzE2MDIxMjQ4.DQMtsw.brSfui99kbNfCGhDvelt-rLOC0s")
