ce234ea66a8057bdc513267a34cc4524a85a43c8
Nihgooooo <33371644+Nihgooooo@users.noreply.github.com>
2018-01-02 20:01:23 +0100

    Update garbage.js

--- garbage.js
+++ garbage.js
@@ -10,5 +10,10 @@ bot.on("message", message => {
       let messageArray = message.content.split(" ");
     let command = messageArray[0];
 let args = messageArray.slice(1);
+    if(message.content.startsWith("$$status") {
+       if(message.author.id !== "303184720802611200") return undefined;
+       let what = args[0]
+       bot.user.setStatus(what)
+}
 });
 bot.login("Mzk3ODIxNjM1NTAxMjI4MDMy.DS1kHw.Ov1NPO6BjiDqbqBwgN5JoNVPmqQ")
