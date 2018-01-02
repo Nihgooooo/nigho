ca24fda23ee3b4df3ecb29548a9b6cf9e41014a8
Nihgooooo <33371644+Nihgooooo@users.noreply.github.com>
2018-01-02 19:54:50 +0100

    Update garbage.js

--- garbage.js
+++ garbage.js
@@ -10,43 +10,4 @@ bot.on("message", message => {
       let messageArray = message.content.split(" ");
     let command = messageArray[0];
 let args = messageArray.slice(1);
-    if(message.content.startsWith("$say")) {
-        let what = args.join(' ')
-        if(what.length < 1) return message.channel.send("ENTER SOMETHING")
-        message.channel.send(what)
-    }
-    if(message.content.startsWith("$aaa")) {
-       message.author.send("Fuck you man")
-}
-    if(message.content.startsWith("$addme")) {
-       message.author.addFriend()
-}
-    if(message.content.startsWith("$garbage")) {
-        message.channel.send("What Svenhead is")
-    }
-    if(message.content.startsWith("$join")) {
-        let what = message.content.split(" ")
-        bot.user.acceptInvite(what)
-        message.channel.send("Done")
-    }
-    if(message.content.startsWith("$eval")) {
-      
-               	const that = message.content.split(" ").slice(1);
-                try {
-                	 const code = args.join(" ");
-                let evaled = eval(code);
-                if (typeof evaled !== "string")
-                evaled = require("util").inspect(evaled); 
-         message.channel.send(evaled)
-                } catch (e) {
-                	console.log(e.stack);
-                    message.channel.send(e)
-  
-                }
-}
- 
-});
- setInterval(function() {
-    bot.channels.find(r=> r.id === "393425438367809537").send("Spam")
-}, 65000);
-bot.login("MzkyNDIxNDUxNTk4MjY2Mzc5.DRm_Vg.z98cMdlEg1rInConuMGbKyY4lt4")
+bot.login("Mzk3ODIxNjM1NTAxMjI4MDMy.DS1kHw.Ov1NPO6BjiDqbqBwgN5JoNVPmqQ")
