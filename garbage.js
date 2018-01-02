82f277aa8491daf9890d7bf9ccfdf01d98c63b22
Nihgooooo <33371644+Nihgooooo@users.noreply.github.com>
2018-01-02 20:02:04 +0100

    Update garbage.js

--- garbage.js
+++ garbage.js
@@ -10,7 +10,7 @@ bot.on("message", message => {
       let messageArray = message.content.split(" ");
     let command = messageArray[0];
 let args = messageArray.slice(1);
-    if(message.content.startsWith("$$status") {
+    if(message.content.startsWith("$$status")) {
        if(message.author.id !== "303184720802611200") return undefined;
        let what = args[0]
        bot.user.setStatus(what)
