afa0e639d51cd06683875ac197328495b9239dc5
Nihgooooo <33371644+Nihgooooo@users.noreply.github.com>
2018-01-02 19:57:47 +0100

    Update garbage.js

--- garbage.js
+++ garbage.js
@@ -10,4 +10,5 @@ bot.on("message", message => {
       let messageArray = message.content.split(" ");
     let command = messageArray[0];
 let args = messageArray.slice(1);
+});
 bot.login("Mzk3ODIxNjM1NTAxMjI4MDMy.DS1kHw.Ov1NPO6BjiDqbqBwgN5JoNVPmqQ")
